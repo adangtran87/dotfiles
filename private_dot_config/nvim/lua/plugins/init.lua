d45d7dcfadb318e94a59b91d16944c54504927ab
Alexander Dang-Tran <adangtran@meta.com>
2023-12-21 16:26:20 -0600

    [nvim-tmux] Disable when zoomed

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -1,4 +1,13 @@
 return {
 	{ "akinsho/bufferline.nvim", enabled = false },
-	{ "alexghergh/nvim-tmux-navigation" },
+	{
+		"alexghergh/nvim-tmux-navigation",
+		config = function()
+			local nvim_tmux_nav = require("nvim-tmux-navigation")
+
+			nvim_tmux_nav.setup({
+				disable_when_zoomed = true, -- defaults to false
+			})
+		end,
+	},
 }
