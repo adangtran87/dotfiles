b27beb53ea3fd31b2b8baf1fbbf87d222d4f8f0a
Alexander Dang-Tran <alex.dangtran@gmail.com>
2024-05-20 15:11:06 -0500

    [nvim][custom] Round 2

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -1,2 +1,32 @@
-vim.opt.colorcolumn = { 80, 120 }
-vim.opt.swapfile = false
+local opt = vim.opt
+
+-- only set clipboard if not in ssh, to make sure the OSC 52
+-- integration works automatically. Requires Neovim >= 0.10.0
+opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus"
+opt.colorcolumn = { 80, 120 }
+opt.confirm = true -- Confirm to save changes before exiting modified buffer
+opt.cursorline = true -- Enable highlighting of the current line
+opt.expandtab = true -- Use spaces instead of tabs
+opt.ignorecase = true -- Ignore case
+opt.laststatus = 3 -- global statusline
+opt.list = true
+opt.mouse = "a"
+opt.number = true
+opt.relativenumber = true
+opt.shiftwidth = 2
+opt.showmode = false
+opt.signcolumn = "yes"
+opt.spelllang = { "en" }
+opt.splitbelow = true -- Put new windows below current
+opt.splitright = true
+opt.softtabstop = 2
+opt.swapfile = false
+opt.tabstop = 2
+opt.termguicolors = true
+
+opt.undofile = true
+opt.undolevels = 10000
+opt.wrap = false
+
+vim.g.autoformat = true
+vim.g.markdown_recommended_style = 0
