d22ace235b81ef1570bb066a463f80d3e850883a
Alexander Dang-Tran <alex.dangtran@gmail.com>
2024-05-27 16:08:07 -0500

    [nvim][custom] fix tab to whitespace

--- private_dot_config/nvim/lua/custom/plugins/which-key.lua
+++ private_dot_config/nvim/lua/custom/plugins/which-key.lua
@@ -1,12 +1,12 @@
 return {
-	{
-		"folke/which-key.nvim",
-		opts = {
-			plugins = { spelling = true },
-		},
-		config = function(_, opts)
-			local wk = require("which-key")
-			wk.setup(opts)
-		end,
-	},
+  {
+    "folke/which-key.nvim",
+    opts = {
+      plugins = { spelling = true },
+    },
+    config = function(_, opts)
+      local wk = require("which-key")
+      wk.setup(opts)
+    end,
+  },
 }
