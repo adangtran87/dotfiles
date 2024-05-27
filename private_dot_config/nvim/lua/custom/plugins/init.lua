006105f76cab120e894b5b194131e9d472abac18
Alexander Dang-Tran <alex.dangtran@gmail.com>
2024-05-27 16:08:27 -0500

    [nvim][custom] add undotree

--- private_dot_config/nvim/lua/custom/plugins/init.lua
+++ private_dot_config/nvim/lua/custom/plugins/init.lua
@@ -1,12 +1,20 @@
 return {
-	{
-		"alexghergh/nvim-tmux-navigation",
-		config = function()
-			local nvim_tmux_nav = require("nvim-tmux-navigation")
+  {
+    "alexghergh/nvim-tmux-navigation",
+    config = function()
+      local nvim_tmux_nav = require("nvim-tmux-navigation")
 
-			nvim_tmux_nav.setup({
-				disable_when_zoomed = true, -- defaults to false
-			})
-		end,
-	},
+      nvim_tmux_nav.setup({
+        disable_when_zoomed = true, -- defaults to false
+      })
+    end,
+  },
+  { "mbbill/undotree",
+    config = function()
+      local wk = require("which-key")
+      wk.register({
+        ["<leader>u"] = { ":UndotreeToggle<cr>", "Undotree" },
+      })
+    end,
+  },
 }
